3480bcc9dd9d957a01e4cd2ee819ceb0e91c140f
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-03-19 16:25:38 -0700

    docs

---
+++ docs/_flywheel_8py.js
@@ -0,0 +1,5 @@
+var _flywheel_8py =
+[
+    [ "start_flywheel", "_flywheel_8py.html#aa35397ddb1503c5f8c1118ad27d812a8", null ],
+    [ "stop_flywheel", "_flywheel_8py.html#ad1fdec2efd295f87343d0328d9ba7581", null ]
+];
--- docs/flywheel_8py.js
+++
@@ -1,5 +0,0 @@
-var flywheel_8py =
-[
-    [ "start_flywheel", "flywheel_8py.html#aa35397ddb1503c5f8c1118ad27d812a8", null ],
-    [ "stop_flywheel", "flywheel_8py.html#ad1fdec2efd295f87343d0328d9ba7581", null ]
-];
