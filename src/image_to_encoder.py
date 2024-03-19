45f215fb57653ab7816a9d85dff7d11f7b78aa1d
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-03-19 12:58:53 -0700

    Update image_to_encoder.py

--- src/image_to_encoder.py
+++ src/image_to_encoder.py
@@ -2,7 +2,7 @@
 Script Name: image_to_encoder.py
 Description: This script defines a class to interface with an MLX90640 thermal infrared camera for image capture and analysis.
 
-Author: Dr. Ridgley
+Author: Dr. Ridgely
 
 Modified by: Conor Schott, Fermin Moreno, Berent Baysal
 Date: 3/14/2024
@@ -20,14 +20,14 @@ from mlx90640.calibration import NUM_ROWS, NUM_COLS, TEMP_K
 from mlx90640.image import ChessPattern, InterleavedPattern
 
 class MLX_Cam:
-    """
+    """!
     @brief   Class which wraps an MLX90640 thermal infrared camera driver to
              make it easier to grab and use an image.
-    !"""
+    """
 
     def __init__(self, i2c, address=0x33, pattern=ChessPattern,
                  width=NUM_COLS, height=NUM_ROWS):
-        """
+        """!
         @brief   Set up an MLX90640 camera.
         @param   i2c An I2C bus which has been set up to talk to the camera;
                  this must be a bus object which has already been set up
@@ -36,7 +36,7 @@ class MLX_Cam:
                  the pixels at a time (default ChessPattern)
         @param   width The width of the image in pixels; leave it at default
         @param   height The height of the image in pixels; leave it at default
-        !"""
+        """
         ## The I2C bus to which the camera is attached
         self._i2c = i2c
         ## The address of the camera on the I2C bus
@@ -57,13 +57,13 @@ class MLX_Cam:
         self._image = self._camera.raw
 
     def ascii_art(self, array):
-        """
+        """!
         @brief   Show a data array from the IR image as ASCII art.
         @details Each character is repeated twice so the image isn't squished
                  laterally. A code of "><" indicates an error, probably caused
                  by a bad pixel in the camera. 
         @param   array The array to be shown, probably @c image.v_ir
-        !"""
+        """
         asc = " -.:=+*#%@"
         scale = len(asc) / (max(array) - min(array))
         offset = -min(array)
@@ -81,7 +81,7 @@ class MLX_Cam:
         return
 
     def get_image(self):
-        """
+        """!
         @brief   Get one image from a MLX90640 camera.
         @details Grab one image from the given camera and return it. Both
                  subframes (the odd checkerboard portions of the image) are
@@ -90,7 +90,7 @@ class MLX_Cam:
                  that the camera is in the ChessPattern (default) mode as it
                  probably should be.
         @returns A reference to the image object we've just filled with data
-        !"""
+        """
         for subpage in (0, 1):
             while not self._camera.has_data:
                 time.sleep_ms(50)
@@ -100,7 +100,7 @@ class MLX_Cam:
         return image
 
     def find_hotSpot(self, array):
-        """
+        """!
         @brief   Find the hottest average cluster of 1x4 pixels in the image.
         @details Each pixel in the image is checked and its value is used to
                  compute a local average (in groups of four). These averages
@@ -109,7 +109,7 @@ class MLX_Cam:
         @param   array The array to be shown, probably @c image
         @returns A set of coordinates pertaining to the hottest average cluster,
                  with the assumption that the array is 24x32.
-        !"""
+        """
         cluster_temps = []
         for row in range(self._height - 1):
             for col in range(self._width - 3):
@@ -120,11 +120,11 @@ class MLX_Cam:
         return [x, y]
 
     def hotspot_to_encoder_position(self, hotspot_location, total_hotspot_locations):
-        """
+        """!
         @brief   Convert hotspot location to encoder position.
         @param   hotspot_location Hotspot location
         @returns Encoder position
-        !"""
+        """
         # Define the arrays for hotspot locations and corresponding encoder positions
         hotspots = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         encoders = [-25475, -25648, -25822, -25995, -26169, -26343, -26516, -26690, -26864, -27037, -27211, -27385, -27558, -27732, -27906, -28079]
@@ -140,11 +140,11 @@ class MLX_Cam:
         return encoders[idx]
 
     def hotspot_to_encoder(self, hotspot_location):
-        """
+        """!
         @brief   Convert hotspot location to encoder position.
         @param   hotspot_location Hotspot location
         @returns Encoder position
-        !"""
+        """
         total_hotspot_locations = self._width * (self._height - 1) * 3
         encoder_position_a = self.hotspot_to_encoder_position(hotspot_location, total_hotspot_locations)
         return encoder_position_a
